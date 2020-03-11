b6ee8a1e341af14abd8fafaf7e270c46f2b3afd9
Alanas Jakubauskas <alanasjakubauskas@gmail.com>
2020-03-11 22:12:10 +0000

    end of today

--- java/src/example/WinXP.java
+++ java/src/example/WinXP.java
@@ -1,5 +1,6 @@
 package example;
 
+
 import processing.core.*;
 
 // This is an example of a visual that renders the waveform
@@ -14,44 +15,88 @@ public class WinXP
     public WinXP(MyVisual mv)
     {
         this.mv = mv;
-        audioMaxHeight = mv.height/5;
+        audioMaxHeight = mv.height/8;
 
     }
 
 
     public void render()
     {
+        
         mv.colorMode(PApplet.HSB);
         mv.noStroke();
-        mv.fill(0,20);
+        mv.fill(0,10);
         mv.rect(0,0,mv.width,mv.height);
-        mv.copy(0,0,mv.width,mv.height,10,10,mv.width,mv.height);
+        mv.copy(0,0,mv.width,mv.height,5,5,mv.width,mv.height);
 
         // random counter variable
         mv.counter++;
         mv.translate(mv.height/2, mv.width/2);
-        mv.fill(mv.counter%255,100,255,100);
+        mv.scale(MyVisual.map(mv.lerpedAmplitude,0f,1f,0.9f,1.5f));
 
-        if(mv.amplitude > .2f){
-           // mv.circle(0, 0, audioWaveWidth);
-        }
+        
      
 
         mv.rotate(mv.counter/60);
-        //mv.circle(0,audioWaveWidth/2, audioWaveWidth*mv.amplitude);
-        //mv.circle(0,-audioWaveWidth/2, audioWaveWidth*mv.amplitude);
 
+        if(mv.amplitude > .1f){
+            float oldAmp=0;
+            for(int i = 0 ; i < 15 ; i ++) // use mv.ab.size() 1024 for whole buffer
+            {
+                mv.stroke(mv.counter%255,200,255,(int) MyVisual.map(mv.amplitude,0f,.4f,0f,255f));
+                
+                
+                float x1 = MyVisual.map(i, 0, 15, -audioWaveWidth / 2, audioWaveWidth / 2);
+                float x2 = MyVisual.map(i+1, 0, 15, -audioWaveWidth / 2, audioWaveWidth / 2);
+                
+                mv.line(x1,0, (x1+x2)/2,-audioMaxHeight * mv.ab.get( (int) MyVisual.map(i,0,25,1,1024)));
+                mv.line((x1+x2)/2,-audioMaxHeight * mv.ab.get( (int) MyVisual.map(i,0,25,1,1024)), x2, 0);
 
-        for(int i = 0 ; i < audioWaveWidth ; i ++) // use mv.ab.size() 1024 for whole buffer
-        {
-            mv.stroke(
-                PApplet.map(i, 0, audioWaveWidth, 0, 255)
-                , 255
-                , 255
-            );
+                //mv.line(x1, y1, x2, y2);
+                
+                oldAmp = mv.ab.get(i);
+                //prev
+    
+                //mv.line(i-audioWaveWidth/2, -audioMaxHeight * mv.ab.get(i), i-audioWaveWidth/2, audioMaxHeight * mv.ab.get(i));
+                
+            }
 
-            mv.line(i-audioWaveWidth/2, -audioMaxHeight * mv.ab.get(i), i-audioWaveWidth/2, audioMaxHeight * mv.ab.get(i));
-            
+            mv.fill(mv.counter%255,140,255,255);
+            mv.noStroke();
+            mv.circle(audioWaveWidth/2,0, audioWaveWidth*mv.amplitude/2);
+            mv.circle(-audioWaveWidth/2,0, audioWaveWidth*mv.amplitude/2);
         }
+        else{
+
+            mv.fill(mv.counter%255,100,255,255);
+            mv.noStroke();
+            mv.circle(audioWaveWidth/2,0, audioWaveWidth*mv.amplitude);
+            mv.circle(-audioWaveWidth/2,0, audioWaveWidth*mv.amplitude);
+
+
+        }
+ 
+
+        /*
+        if(mv.amplitude > .2f){
+            for(int i = 0 ; i < audioWaveWidth ; i ++) // use mv.ab.size() 1024 for whole buffer
+            {
+                mv.stroke(
+                    PApplet.map(i, 0, audioWaveWidth, 0, 255)
+                    , 255
+                    , 255
+                    , 100
+                );
+    
+                mv.line(i-audioWaveWidth/2, -audioMaxHeight * mv.ab.get(i), i-audioWaveWidth/2, audioMaxHeight * mv.ab.get(i));
+                
+            }
+        }
+        */
+
+        
+
+        
+       
     }
 }
